8a1845a694a476960297f7f4bceac5ebb0a97bee
Md Arafat <130009990+mdearafat@users.noreply.github.com>
2024-10-21 09:30:33 -0400

    create static version

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        primary: {
+          50: '#F5F7FA',
+          100: '#EAEFF4',
+          200: '#D1DCE6',
+          300: '#A8BDD1',
+          400: '#799AB7',
+          500: '#597E9F',
+          600: '#456584',
+          700: '#39516B',
+          800: '#32465A',
+          900: '#2D3C4D',
+          950: '#1E2833',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
